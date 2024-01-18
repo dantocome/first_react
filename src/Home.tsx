243e2892602b7f444e3d49579a03660af3313f56
Daniel Egele <danielegele9@gmail.com>
2024-01-18 13:46:02 +0100

    all added files

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,29 @@
 import React from "react";
+import logo from "./images/logo.webp"
+import "./Home.css"
 
 
 function Home(){
     return(
         <>
         <header>
+            <div className="log">
+             <img className="img1" src={logo} alt="" />
+            </div>
             <nav>
-                <ul>
+                <ul className="list">
+                    <div className="home">
                     <li><a href="">Home</a></li>
                     <li><a href="">Services</a></li>
                     <li><a href="">About us</a></li>
+                    </div>
+                    
+                    <div className="bt">
+                    <span><button className="btn">SELECT LANGUAGE</button></span>
+                    </div>
+                    
                 </ul>
+                
             </nav>
         </header>
         </>
