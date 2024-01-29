cdc724f20d446aa6761bc855ce210bb13bec8043
Daniel Egele <danielegele9@gmail.com>
2024-01-29 17:31:02 +0100

    all added files

--- src/component/EmeksRef.tsx
+++ src/component/EmeksRef.tsx
@@ -2,15 +2,39 @@ import React, { useRef } from 'react';
 import "./Emeks.css"
 
 const EmeksRef = () => {
-const paymentRef = useRef("");
+const paymentstartdateRef = useRef("");
 const amountRef = useRef("");
 const enddateRef = useRef("");
 const rateRef = useRef("");
 const monthsRef = useRef("");
 const nameRef = useRef("");
+const users = {paymentstartdate:"", amount:"", enddate:"", rate:"", months:"", name:"",}
 
 const handleSubmit = (event)=>{
     event.preventDefault();
+
+    if(paymentstartdateRef.current !==null){
+      users.paymentstartdate = paymentstartdateRef.current.value;
+    }
+    if(amountRef.current !==null){
+      users.amount = amountRef.current.value;
+    }
+    if(enddateRef.current !==null){
+      users.enddate = enddateRef.current.value;
+    }
+    if(rateRef.current !==null){
+      users.rate = enddateRef.current.value;
+    }
+    if(monthsRef.current !==null){
+      users.months = monthsRef.current.value;
+    }
+    if(nameRef.current !==null){
+      users.name = nameRef.current.value;
+    }
+    console.log(users);
+    
+
+
 
 
 }
@@ -34,43 +58,48 @@ const handleSubmit = (event)=>{
 
     
     <div className="main">
-    <form >
+    <form onSubmit={handleSubmit}>
           <div className="application">
             <h3>Loan Application Form</h3>
           </div>
         <div className="for">
             <div className="form-one-end">
             <div className="form-one">
-            <label htmlFor="" className='name'>Loan Payment Start Date</label><br></br>
-            <input type="text" />
+            <label htmlFor="Loan payment start date" className='name'>Loan Payment Start Date</label><br></br>
+            <input id='date' type="text" ref={paymentstartdateRef}/>
             </div>
             <div className="form-one">
-            <label htmlFor="" className='name'>Loan Aount</label><br></br>
-            <input type="text" />
+            <label htmlFor="Loan amount" className='name'>Loan Amount</label><br></br>
+            <input id='number' type="text" ref={amountRef}/>
             </div>
             </div>
             <div className="form-one-end">
             <div className="form-one">
-            <label htmlFor="" className='name'>Loan Payment End Date</label><br></br>
-            <input type="text" />
+            <label htmlFor="loan payment end date" className='name'>Loan Payment End Date</label><br></br>
+            <input id='date' type="text" ref={enddateRef}/>
             </div>
             <div className="form-one">
-            <label htmlFor="" className='name'>Loan Rate</label><br></br>
-            <input type="text" />
+            <label htmlFor="loan rate" className='name'>Loan Rate</label><br></br>
+            <input id='number' type="text" ref={rateRef}/>
             </div>
             </div>
             <div className="form-one-end">
             <div className="form-one">
-            <label htmlFor="" className='name'>Number of Months</label><br></br>
-            <input type="text" />
+            <label htmlFor="number of months" className='name'>Number of Months</label><br></br>
+            <input id='number' type="text" ref={monthsRef}/>
             </div>
             <div className="form-one">
-            <label htmlFor="" className='name'>Name of Borrower</label><br></br>
-            <select name="" id=""> <option value=""><input type="text" /></option></select>
+            <label htmlFor="name of borrower" className='name'>Name of Borrower</label><br></br>
+            <select name="text" id="name">
+            <input type="text"  placeholder='select customers name' ref={nameRef}/>
+              <option value="">select customers name</option>
+              <option value="">David Clark</option>
+            </select>
            
             </div>
             </div>
         </div>
+        <button type='submit'>Submit</button>
 
     </form>
     </div>
